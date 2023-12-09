ad906eb326d4d7df365cb29a22c9192185f9f443
Kermabon Stéphane <demis27@demis27.net>
2023-12-09 17:06:05 +0100

    Day 01

---
+++ day01.py
@@ -0,0 +1,29 @@
+def first_step():
+    global open
+    with open('day01.txt') as f:
+        line = f.readline()
+        open = line.count("(")
+        close = line.count(")")
+        print(open - close)
+
+def second_step():
+    global open
+    with open('day01.txt') as f:
+        line = f.readline()
+        inc = 0
+        open = 0
+        for c in line:
+            if c == '(':
+                inc += 1
+                open += 1
+            elif c == ')' and open < 0:
+                print(inc)
+                break
+            else:
+                inc += 1
+                open -= 1
+        f.close()
+
+first_step() #74
+
+second_step() #1795
